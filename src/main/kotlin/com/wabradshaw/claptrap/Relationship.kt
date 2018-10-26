ac940b49cd6db44db52f50b5e31c1dd15763bec3
William Bradshaw <w.a.bradshaw@gmail.com>
2018-10-26 22:02:38 +0700

    Added skeleton for the Relatonship class.

---
+++ src/main/kotlin/com/wabradshaw/claptrap/Relationship.kt
@@ -0,0 +1,37 @@
+package com.wabradshaw.claptrap
+
+/**
+ * A relationship links an original word, and a different substitution word.
+ */
+enum class Relationship {
+
+    /**
+     * The two words mean practically the same thing. E.g. fire == flame.
+     */
+    SYNONYM,
+
+    /**
+     * The original word is a more specific type of the substitution. E.g. cabbage IS_A vegetable.
+     */
+    IS_A,
+
+    /**
+     * The substitution word is a more specific type of the original. E.g. vegetable INCLUDES cabbage.
+     */
+    INCLUDES,
+
+    /**
+     * The original word contains the substitution. E.g. flower HAS_A petal.
+     */
+    HAS_A,
+
+    /**
+     * The substitution word contains the original. E.g. petal PART_OF flower
+     */
+    PART_OF,
+
+    /**
+     * The original word is the opposite of the substitute. Only used for adj/adv. E.g. hot IS_NOT cold.
+     */
+    IS_NOT;
+}
