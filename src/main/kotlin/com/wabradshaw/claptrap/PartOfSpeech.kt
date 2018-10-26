2ca6e8308f5255742e81306bd0959e1f8b7089df
William Bradshaw <w.a.bradshaw@gmail.com>
2018-10-26 21:23:39 +0700

    Added placeholder PartOfSpeech enum

---
+++ src/main/kotlin/com/wabradshaw/claptrap/PartOfSpeech.kt
@@ -0,0 +1,11 @@
+package com.wabradshaw.claptrap
+
+/**
+ * An enum for the different parts of speech (pos) handled by the system. A pos is a tag
+ * that describes the grammatical role of a word, essentially how it can be used in a sentence.
+ */
+enum class PartOfSpeech {
+    NOUN,
+    VERB,
+    ADJECTIVE;
+}
