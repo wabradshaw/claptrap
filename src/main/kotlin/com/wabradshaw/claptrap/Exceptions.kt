5acdc0b258d1819f895fc9f9a0abb16485724f41
William Bradshaw <w.a.bradshaw@gmail.com>
2018-10-27 21:43:24 +0700

    Added an Exceptions file, including a no joke exception, and no substitutes.

---
+++ src/main/kotlin/com/wabradshaw/claptrap/Exceptions.kt
@@ -0,0 +1,6 @@
+package com.wabradshaw.claptrap
+
+open class NoJokeException(message: String): Exception(message)
+
+class NoSubstitutionException(word: String):
+        NoJokeException("Could not create a joke as there is no substitution available for " + word)
