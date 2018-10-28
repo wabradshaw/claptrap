b20411b88e230e34c168d31b0c51a1a4adbb38d1
William Bradshaw <w.a.bradshaw@gmail.com>
2018-10-28 22:50:22 +0700

    Improved the presentation of the NoSubstitutionException by quoting the errant word.

--- src/main/kotlin/com/wabradshaw/claptrap/Exceptions.kt
+++ src/main/kotlin/com/wabradshaw/claptrap/Exceptions.kt
@@ -3,4 +3,4 @@ package com.wabradshaw.claptrap
 open class NoJokeException(message: String): Exception(message)
 
 class NoSubstitutionException(word: String):
-        NoJokeException("Could not create a joke as there is no substitution available for " + word)
+        NoJokeException("Could not create a joke as there is no substitution available for '" + word + "'.")
