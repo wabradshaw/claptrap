30530623651a1ed5b6c92cfde2c203193e5d86d1
William Bradshaw <w.a.bradshaw@gmail.com>
2018-10-28 23:21:44 +0700

    Updated the jokesepc to handle the case where there is no known alternative to the nucleus word, so it will appear twice.

--- src/main/kotlin/com/wabradshaw/claptrap/structure/JokeSpec.kt
+++ src/main/kotlin/com/wabradshaw/claptrap/structure/JokeSpec.kt
@@ -1,6 +1,6 @@
 package com.wabradshaw.claptrap.structure
 
 data class JokeSpec (val nucleus : Word,
-                     val primarySetup : SemanticSubstitution,
+                     val primarySetup : SemanticSubstitution?,
                      val secondarySetup : SemanticSubstitution,
                      val linguisticSub : LinguisticSubstitution)
