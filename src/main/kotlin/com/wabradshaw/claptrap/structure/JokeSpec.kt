a06f76891e8b0e37a1b00a343ffb6e6abcb81368
William Bradshaw <w.a.bradshaw@gmail.com>
2018-10-29 11:55:08 +0700

    Adapted the JokeSpec to handle situations where the nucleus isn't a known word.

--- src/main/kotlin/com/wabradshaw/claptrap/structure/JokeSpec.kt
+++ src/main/kotlin/com/wabradshaw/claptrap/structure/JokeSpec.kt
@@ -1,6 +1,7 @@
 package com.wabradshaw.claptrap.structure
 
-data class JokeSpec (val nucleus : Word,
+data class JokeSpec (val nucleus : String,
+                     val nucleusWord : Word?,
                      val primarySetup : SemanticSubstitution?,
                      val secondarySetup : SemanticSubstitution,
                      val linguisticSub : LinguisticSubstitution)
