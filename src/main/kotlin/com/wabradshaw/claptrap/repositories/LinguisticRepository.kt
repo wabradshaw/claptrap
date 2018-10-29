df087756bd7de997b785c973cabd9e85bb018faf
William Bradshaw <w.a.bradshaw@gmail.com>
2018-10-29 11:58:56 +0700

    Added the interface for linguistic substitutions.

---
+++ src/main/kotlin/com/wabradshaw/claptrap/repositories/LinguisticRepository.kt
@@ -0,0 +1,23 @@
+package com.wabradshaw.claptrap.repositories
+
+import com.wabradshaw.claptrap.structure.*
+
+/**
+ * A LinguisticRepository is an interface for connectors to different dictionaries that contain linguistic links
+ * between words. That is, words which can be substituted for an original because they are similar linguistically. For
+ * example, the two words rhyme or sound similar.
+ */
+interface LinguisticRepository {
+
+    /**
+     * Gets a list of all of the words in the dictionary that can be substituted for the supplied original word,
+     * provided they share one of the supplied linguistic similarities. If no such words exist in the dictionary, then
+     * an empty list is returned.
+     *
+     * @param word The original word to be substituted.
+     * @param validSimilarities A list of all of the Similarities that constitute valid substitutions.
+     * @return A list of the possible LinguisiticSubstitutions for the original word.
+     */
+    fun getSubstitutions(word: Word, validSimilarities: List<LinguisticSimilarity>): List<LinguisticSubstitution>
+
+}
