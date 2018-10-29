499aae26a8a1daaaca90c6ab9bc7c1ec2fbf1131
William Bradshaw <w.a.bradshaw@gmail.com>
2018-10-29 22:32:33 +0700

    Updated javadoc to go along with the new name

--- src/main/kotlin/com/wabradshaw/claptrap/repositories/DictionaryRepository.kt
+++ src/main/kotlin/com/wabradshaw/claptrap/repositories/DictionaryRepository.kt
@@ -10,7 +10,7 @@ interface DictionaryRepository {
     /**
      * Gets the Word object for a particular spelling of a word from the dictionary.
      *
-     * @param word The spelling of the word to lookup.
+     * @param string The spelling of the word to lookup.
      * @return The details of the word, or null if the word doesn't exist in the database.
      */
     fun getWord(string: String ): Word?
