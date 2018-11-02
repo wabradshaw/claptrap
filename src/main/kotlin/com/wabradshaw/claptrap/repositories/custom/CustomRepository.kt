b8f745a3693417afbf94a97f884867b49642436f
William Bradshaw <w.a.bradshaw@gmail.com>
2018-11-02 11:34:58 +0700

    Added comments to the CustomRepository.

--- src/main/kotlin/com/wabradshaw/claptrap/repositories/custom/CustomRepository.kt
+++ src/main/kotlin/com/wabradshaw/claptrap/repositories/custom/CustomRepository.kt
@@ -2,14 +2,36 @@ package com.wabradshaw.claptrap.repositories.custom
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
-import com.sun.org.apache.regexp.internal.RE
 import com.wabradshaw.claptrap.repositories.DictionaryRepository
 import com.wabradshaw.claptrap.repositories.LinguisticRepository
 import com.wabradshaw.claptrap.repositories.SemanticRepository
 import com.wabradshaw.claptrap.structure.*
 import java.io.InputStream
 
-class CustomRepository(private val jsonSource: InputStream,
+/**
+ * A CustomRepository is a dictionary that uses a custom json dictionary to store relationships between words. As this
+ * is a manually created dictionary, it is only suitable for substring matches, not for accepting any word the user can
+ * think of.
+ *
+ * Json is supplied in the following structure:
+ *
+ * [
+ *   {
+ *      "spelling": "cat", // how the word is spelled
+ *      "group": "_at", // the linguistic group for the word
+ *      "pos": "Noun", // the part of speech for the word. Can be "Noun", "Verb", "Adj" or anything else for unknown
+ *      "adult": false, // true if the word would be unsuitable for children
+ *      "has": ["whiskers", "a tail"] // a list of descriptions for what a noun has. Can be empty.
+ *   },
+ *   { ... }
+ * ]
+ *
+ * @param jsonSource An input stream pointing to the json containing the dictionary. Defaults to the standard one.
+ * @param showAdult Whether or not adult words can be included in jokes. Defaults to true.
+ * @param validRelationships A list of the semantic relationships that can be used in jokes. Defaults to all of those
+ *                           included in the default json.
+ */
+class CustomRepository(jsonSource: InputStream = object{}.javaClass.getResourceAsStream("/customDictionary.json"),
                        private val showAdult: Boolean = true,
                        private val validRelationships: List<Relationship>
                        = listOf(Relationship.HAS_A))
@@ -34,6 +56,9 @@ class CustomRepository(private val jsonSource: InputStream,
         return validRelationships.flatMap{relationship -> getRelationship(detailedWord, relationship)}
     }
 
+    /**
+     * Gets all of the substitutions for the given relationship.
+     */
     private fun getRelationship(detailedWord: WordMappingDTO, relationship: Relationship): Iterable<SemanticSubstitution> {
         return when(relationship){
             Relationship.HAS_A -> detailedWord.has.map{SemanticSubstitution(Word(it, it, PartOfSpeech.UNKNOWN, 100.0), detailedWord.toWord(), relationship)}
