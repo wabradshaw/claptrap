f03411299845dd75f320c5a9385fb005fe9a4301
William Bradshaw <w.a.bradshaw@gmail.com>
2019-01-10 11:25:43 +0000

    Added comments to the Determiner manager.

--- src/main/kotlin/com/wabradshaw/claptrap/generation/DeterminerManager.kt
+++ src/main/kotlin/com/wabradshaw/claptrap/generation/DeterminerManager.kt
@@ -14,11 +14,19 @@ class DeterminerManager {
     /**
      * Takes a string and removes any determiner at the very start of it. It will not remove non-determiners, or
      * determiners in the middle of the string.
+     *
+     * @param string The string representation of the word
      */
     fun removeDeterminer(string: String): String {
         return determiners.fold(string) {string, det -> Regex("^$det ").replace(string, "")}
     }
 
+    /**
+     * Finds the default determiner that should be applied to a particular word. Typically this means an "a" for a word
+     * with a consonant sound, and an "an" for a vowel sound.
+     *
+     * @param string The string representation of the word
+     */
     fun chooseDefaultDeterminer(string: String): String {
         val start = string.subSequence(0, 3)
         val cleaned = start.replace(Regex("[^a-zA-Z]"), "*")
