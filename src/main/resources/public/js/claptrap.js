4fc7b340554dc706c443afab1b4db2b716009e41
William Bradshaw <w.a.bradshaw@gmail.com>
2019-01-16 21:02:22 +0000

    Initially working version of the explanation view. Needs determiners and a service.

--- src/main/resources/public/js/claptrap.js
+++ src/main/resources/public/js/claptrap.js
@@ -1,5 +1,57 @@
+function getLinguisticDescriptor(spec){
+	var nucleus = spec.nucleus;
+	var original = spec.linguisticOriginal;
+	var splitNucleus = nucleus.replace(original, "-" + original + "-")
+							  .replace(/--/g,"-")
+							  .replace(/ -|- /g," ")
+							  .replace(/^-|-$/g,"");
+	var formattedResult = spec.punchline.replace(/A |An /,"")
+									    .replace("!","");
+	return splitNucleus + " sounds like " + formattedResult
+}
+
+function getSemanticDescriptor(original, sub, relationship){
+	switch(relationship) {
+		case "SYNONYM": return original + " is the same as "+ sub;
+		case "NEAR_SYNONYM": return original + " is similar to "+ sub;
+		case "IS_A": return original + " is a type of "+ sub;
+		case "INCLUDES": return sub + " is a type of "+ original;
+		case "HAS_A": return original + " has "+ sub;
+		case "PART_OF": return sub + " has a "+ original;
+		case "IN": return original + " can be found in "+ sub;
+		case "ON": return original + " can be found on "+ sub;
+		case "FROM": return original + " comes from "+ sub;
+		case "PROPERTY": return original + " are "+ sub;
+		case "ACTION": return original + " can "+ sub;
+		case "ACTION_CONTINUOUS": return original + " can be "+ sub;
+		case "RECIPIENT_ACTION": return "someone can " + sub + " " + original;
+		case "RECIPIENT_ACTION": return "someone could have " + sub + " " + original;
+		default: return original + " is related to " + sub;
+	}
+}
+
 /**
- * Knockout Data class for a blog.
+ * Knockout Data class for a relationship within a joke.
+ */ 
+function Relationship(token, descriptor, original, substitution, relationship, position) {
+	var self = this;
+	self.token = token;
+	self.descriptor = descriptor;
+	self.original = original;
+	self.substitution = substitution;
+	self.relationship = relationship;
+	
+	self.wrong = ko.observable(false);
+	
+	self.toggleRelationship = function(){
+		self.wrong(!self.wrong());
+		$(":focus").blur();
+		console.log("todo");		
+	}
+}
+ 
+/**
+ * Knockout Data class for a joke.
  */
 function Joke(data, token){
 	var self = this;
@@ -22,6 +74,13 @@ function Joke(data, token){
 		$('#bad-rating').blur()
 	}
 
+	self.relationships = ko.observableArray([]);
+	self.relationships.push(new Relationship(self.token, getLinguisticDescriptor(self.data), self.data.nucleus, self.data.linguisticReplacement, "RHYME", "linguistic"));
+	if(!self.data.primarySetup.endsWith(self.data.nucleus)){
+		self.relationships.push(new Relationship(self.token, getSemanticDescriptor(self.data.nucleus, self.data.primarySetup, self.data.primaryRelationship), self.data.linguisticReplacement, self.data.secondarySetup, self.data.secondaryRelationship, "primary"));
+	}
+	self.relationships.push(new Relationship(self.token, getSemanticDescriptor(self.data.linguisticReplacement, self.data.secondarySetup, self.data.secondaryRelationship), self.data.linguisticReplacement, self.data.secondarySetup, self.data.secondaryRelationship, "secondary"));
+	
 	function rateJoke(){
 		$.ajax({
 			type: 'POST',
@@ -57,7 +116,7 @@ function Joke(data, token){
 }
 
 /**
- * Main Knockout view model containing the trip history.
+ * Main Knockout view model.
  */
 function JokingViewModel(){
 	var self = this;
